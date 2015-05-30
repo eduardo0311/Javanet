69b348962ff8206a0dd175c0f8ec3aa0036c358f
titovna <erika@erika-PC>
2015-05-29 20:42:10 -0500

    cambios en servicios

---
+++ 201216064/JavaNetServices/GestionarSolicitud.svc.cs
@@ -0,0 +1,30 @@
+﻿using JavaNetServices.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace JavaNetServices
+{
+    
+    public class GestionarSolicitud : IGestionarSolicitud
+    {
+
+        public Solicitud GenerarSolicitud(int codCliente, int codProyecto, List<Dominio.SolicitudDetalle> detalleSolicitud)
+        {
+            Solicitud solicitudACrear = new Solicitud();
+            //validar estado de cliente
+
+            //validar estado de Proyecto
+
+            //crear solicitud
+            //obtener solicitud
+
+            //crear detalle de solicitud
+
+            return solicitudACrear;
+        }
+    }
+}
---
+++ 201216064/JavaNetServices/IGestionarSolicitud.cs
@@ -0,0 +1,18 @@
+﻿using JavaNetServices.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace JavaNetServices
+{
+    
+    [ServiceContract]
+    public interface IGestionarSolicitud
+    {
+        [OperationContract]
+        Solicitud GenerarSolicitud(int codCliente, int codProyecto, List<SolicitudDetalle> detalleSolicitud);
+    }
+}
